de6c593fb7dac0c6ea755a6904bb8ddc477fae2d
K4USTIC <cesarmolinaaraya31@gmail.com>
2025-04-21 13:30:02 -0400

    avances de la clase de arreglos

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,6 @@
 #hola soy un comentario
+frutas = ["manzana" , "pera" , "frutilla"]
+print (frutas[1])
+
+estudiante = [{'nombre': "Maxi" , 'edad': 20, 'curso': "2A"} {'nombre': "Richard" , 'edad': 19, 'curso : 1B'}]
+print(estudiante)
