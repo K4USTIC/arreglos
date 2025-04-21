58361315ac865a1b52d4147ebb4ab7150e133d7a
K4USTIC <cesarmolinaaraya31@gmail.com>
2025-04-21 13:57:55 -0400

    modificacion de mi codigo

--- arreglos.py
+++ arreglos.py
@@ -3,4 +3,4 @@ frutas = ["manzana" , "pera" , "frutilla"]
 print (frutas[1])
 
 estudiante = [{'nombre': "Maxi" , 'edad': 20, 'curso': "2A"} {'nombre': "Richard" , 'edad': 19, 'curso : 1B'}]
-print(estudiante)
+print(estudiante [1], ['nombre'])
