93d1b3e66ca6d8348ebc4ff513efe371b7bc75cd
K4USTIC <cesarmolinaaraya31@gmail.com>
2025-04-23 11:18:32 -0400

    nuevo arreglo

---
+++ arreglos2.py
@@ -0,0 +1,21 @@
+playlistDelCurso = [
+    "Beat-it.mp3" , "Loba.mp3" , "15B.mp3" , "Diva Virtual.mp3" , "Pokemon Johtho.mp3" , "Despacito.mp3"
+]
+print(playlistDelCurso)
+print("######")
+print(playlistDelCurso[3])
+print("TAMAÑO DE LA LISTA")
+listSize = len(playlistDelCurso)
+print(listSize)
+print("#######--LA ULTIMA CANCION ES--########")
+print(playlistDelCurso[listSize - 1])
+
+print("otro ejemplo")
+palabra = "maximiliano"
+print(len(palabra))
+
+print("----IMPRIMIR TODAS LAS LETRAS DE UNA PALABRA----")
+nuevaPalabra = "reconocer"
+palabra3 = "bebesita"
+for i in range(len(nuevaPalabra)):
+    print(nuevaPalabra[i])
